a64e13df1ed143a7af9b414739ddbf74dd64ff0f
Divyareddy77 <vangadivyareddy1210@gmail.com>
2026-06-28 12:20:48 +0530

    feat : add JavaScript event handling, callbacks and form validation examples

--- myscript.js
+++ myscript.js
@@ -16,3 +16,59 @@ function StudentMarksCalculation(maths,science,social){
 }
 let {sum,avg}=StudentMarksCalculation(90,80,70);
 console.log(`Total marks = ${sum} Average marks = ${avg}`);
+  
+
+
+
+//Type conversion
+let a = "10";
+let b = Number(a);
+console.log(b , typeof(b));
+
+
+//Type coersion
+let x = "10";
+let y = 20;
+console.log(x + y);
+console.log(x * y);
+
+
+
+//Diff between = & == & ===
+
+let n1 = 10;
+console.log(n1);
+
+
+let n2 = "20";
+let n3 = 20;
+console.log(n2 == n3);
+
+
+
+console.log(n2 === n3);
+let n4 = 20;
+console.log(n3 === n4);
+
+
+
+//call back function
+
+function greet(name){
+    console.log(`Hello ${name}` );
+
+}
+
+function user(callback){
+    callback("Divya");
+}
+ user(greet);
+
+//-------------------------------//
+ function message(){
+    console.log("Welcome ");
+ }
+ setTimeout(message,3000);
+
+
+ 
